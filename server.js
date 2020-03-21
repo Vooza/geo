e2c54fb0a3a50ea271952e1f8ac5b4e6986360a9
Vooza <43589007+Vooza@users.noreply.github.com>
2020-03-20 20:07:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (request, response) => {
-  response.send("aa");
+  response.send(equest.headers.host);
 });
 
 const listener = app.listen(process.env.PORT, () => {
