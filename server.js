9b53809ead315f48ffcb7f12dbd7984557cac4fc
Vooza <43589007+Vooza@users.noreply.github.com>
2020-03-20 19:56:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hehe");
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.send("aa");
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
